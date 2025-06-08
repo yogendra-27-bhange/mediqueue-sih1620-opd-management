70fdfc8910fe777e56d7229fa52a0d6ba385f75d
CRISTOFER xOP <cristoferxop50@gmail.com>
2025-06-08 06:43:31 +0000

    bro fix this

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,6 +1,6 @@
 
 import Link from 'next/link';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle, CardFooter } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Lightbulb, Users, BriefcaseMedical, BedDouble, ListChecks, Settings, Hospital as HospitalIcon, Ambulance } from 'lucide-react';
 
